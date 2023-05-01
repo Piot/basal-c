9dbbdf63930edb18e78444ede009cd836c7c2f4d
Peter Bjorklund <piot@hotmail.com>
2023-05-01 19:11:28 +0200

    fix: remove logging

--- src/lib/line_segment.c
+++ src/lib/line_segment.c
@@ -52,11 +52,6 @@ BlCollision blLineSegmentCircleIntersect(const BlLineSegment segment, const BlCi
 
     float distanceBetweenCircleAndClosestPoint = blVector2Length(deltaCircleCenterAndClosestPoint);
     result.depth = ((float) circle.radius) - distanceBetweenCircleAndClosestPoint;
-    CLOG_VERBOSE("depth is set radius:%f distance:%f depth:%f", circle.radius, distanceBetweenCircleAndClosestPoint,
-                 result.depth)
-    if (result.depth != 0) {
-        CLOG_VERBOSE("depth is set")
-    }
     result.normal = blVector2Scale(deltaCircleCenterAndClosestPoint, 1.0f / distanceBetweenCircleAndClosestPoint);
     result.point = closestPoint;
 
